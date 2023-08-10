2dfa493c15ba3094470ac47d3b6361131bc70416
dongeunshin <7017410@naver.com>
2023-08-10 15:29:03 +0900

    [feat] SuggestionCollectionViewCell 구현

---
+++ Dibivava/Dibivava/Presentation/SupplementDetailScene/View/SuggestionCollectionViewCell.swift
@@ -0,0 +1,85 @@
+//
+//  SuggestionCollectionViewCell.swift
+//  Dibivava
+//
+//  Created by dong eun shin on 2023/08/10.
+//
+
+import UIKit
+import Then
+
+class SuggestionCollectionViewCell: UICollectionViewCell {
+    static let identifier: String = String(describing: SuggestionCollectionViewCell.self)
+    
+    private let imageView: UIImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private let titleLabel: UILabel = UILabel().then {
+        $0.textColor = .black
+        $0.textAlignment = .left
+        $0.font = .pretendard(.Regular, size: 17)
+    }
+    
+    private let companyLabel: BasePaddingLabel = BasePaddingLabel().then {
+        $0.textColor = .black
+        $0.textAlignment = .left
+        $0.layer.cornerRadius = 10
+        $0.clipsToBounds = true
+        $0.font = .pretendard(.Regular, size: 15)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        self.layer.borderWidth = 0.25
+        self.layer.cornerRadius = 10.0
+        self.clipsToBounds = true
+        
+        self.configureSubviews()
+        self.configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.imageView.image = nil
+        self.titleLabel.text = ""
+        self.companyLabel.text = ""
+    }
+    
+    func configure(title: String, companyLabel: String) {
+        self.titleLabel.text = title
+        self.companyLabel.text = companyLabel
+    }
+}
+
+private extension SuggestionCollectionViewCell {
+    func configureSubviews() {
+        [imageView, titleLabel, companyLabel].forEach {
+            self.contentView.addSubview($0)
+        }
+    }
+    
+    func configureConstraints() {
+        self.imageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(10)
+            make.leading.trailing.equalToSuperview().inset(10)
+        }
+        
+        self.titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.imageView.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(10)
+        }
+        
+        self.companyLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.titleLabel.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(10)
+            make.bottom.equalToSuperview().inset(10)
+        }
+    }
+}
