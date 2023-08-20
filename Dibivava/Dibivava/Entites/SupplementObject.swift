c0f691d7fca81213989748b91d50f3b6cf8dffc7
dongeunshin <7017410@naver.com>
2023-08-21 04:06:05 +0900

    [feat] SupplementObject 구현

---
+++ Dibivava/Dibivava/Entites/SupplementObject.swift
@@ -0,0 +1,18 @@
+//
+//  SupplementObject.swift
+//  Dibivava
+//
+//  Created by dong eun shin on 2023/08/21.
+//
+
+import Foundation
+
+struct SupplementObject {
+    let supplementID: Int
+    let name, company, expireDate, intakeMethod: String?
+    let functionality: [String]?
+    let mainMaterial: [String]?
+    let subMaterial, additive: [String]?
+    let imageLink: String?
+    let gmpCheck: Int?
+}
